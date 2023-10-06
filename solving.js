9af1a56f22844d7349495a4e80727fd7876366e1
laithalkhraisha <laithalkhraisha01@gmail.com>
2023-10-06 15:19:25 +0300

    problem_solving_task

---
+++ solving.js
@@ -0,0 +1,63 @@
+// q5)
+// const array=[];
+//  const base=2;
+//  function Exponentiation (){
+//  for(let i=1;i<=4;i++){
+//      let num=prompt("Please enter the number");
+//      array.push(num ** base);
+//  }
+//  console.log(array)
+// }
+
+
+// Q6)
+
+// const array=[];
+// const newarray=[];
+// function pusharray(){
+//     for(let i=0;i<=4;i++){
+//         let values=prompt("Enter the value:");
+//         array.push(values);
+        
+// }
+// array.map(values=>{
+//     if(isNaN(values)){
+//         newarray.push("n/a")
+//     }
+//     else if(values%2==0){
+//         newarray.push("even");    
+// }
+// else if(values%2!=0){
+//     newarray.push("odd"); 
+// }
+// })
+// console.log(array);
+// console.log(newarray);
+// }
+
+
+
+// // q7)
+// const array=[];
+// const newarray=[];
+// function fizzbuzz(){
+//     for(let i=0;i<=4;i++){
+//                 let values=prompt("Enter the value:");
+//                 array.push(values);
+
+// }
+// array.forEach(values=>{
+//     if (values%5==0&&values%3==0) {
+//         newarray.push("Fizz Buzz");
+//     }
+//     else if (values%5==0) {
+//         newarray.push("Buzz");
+//     }
+//     else if (values%3==0) {
+//         newarray.push("Fizz");
+//     }
+
+// })
+// console.log(array);
+// console.log(newarray);
+// }
